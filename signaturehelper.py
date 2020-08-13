07829c1fc5ab3ef233c994564be834c67b8d6978
YGKim-create <62316739+YGKim-create@users.noreply.github.com>
2020-08-13 11:21:09 +0900

    Add files via upload

---
+++ signaturehelper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import hashlib
+import hmac
+import base64
+
+
+class Signature:
+
+    @staticmethod
+    def generate(timestamp, method, uri, secret_key):
+        message = "{}.{}.{}".format(timestamp, method, uri)
+        hash = hmac.new(bytes(secret_key, "utf-8"), bytes(message, "utf-8"), hashlib.sha256)
+
+        hash.hexdigest()
+        return base64.b64encode(hash.digest())
+
+
+# In[ ]:
+
+
+
+
